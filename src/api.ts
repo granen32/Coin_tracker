7e35ff0a6cf04239cc6d447c6d3d26ad5a316523
rnjsrbgud@naver.com <rnjsrbgud@naver.com>
2022-07-15 18:38:30 +0900

    api 수정

--- src/api.ts
+++ src/api.ts
@@ -1,21 +1,21 @@
 // 이 함수는 json .data를 반환해줘야함 ->fetcher 함수는 promise여야함
 import axios from "axios";
-const BaseUrl = `https://api.coinpaprika.com/v1`;
+const BASE_URL = "https://api.coinpaprika.com/v1";
 const PRICEURL = `https://ohlcv-api.nomadcoders.workers.dev/?coinId=`;
 // 이건 함수형으로 해줘야함
 export const fetchCoins = async() =>{
   // 데이터 얻는법
   // axios.get(url).then(res => console.log(res.data))
-  const response = await axios.get(`${BaseUrl}/coins`).then(res => res.data);
+  const response = await axios.get(`${BASE_URL}/coins`).then(res => res.data);
   return response;
 }
 // coin 화면 api
 export const coinUrl = async(coinId:string) =>{
-  const response = await axios.get(`${BaseUrl}/coins/${coinId}`).then(res => res.data);
+  const response = await axios.get(`${BASE_URL}/coins/${coinId}`).then(res => res.data);
   return response;
 }
 export const priceUrl = async(coinId:string) =>{
-  const response = await axios.get(`${BaseUrl}/tickers/${coinId}`).then(res => res.data);
+  const response = await axios.get(`${BASE_URL}/tickers/${coinId}`).then(res => res.data);
   return response;
 }
 // 차트 화면
