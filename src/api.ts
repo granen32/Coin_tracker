a9ab8fb5ce55c5051cb39ef5593ed890b554f3ec
rnjsrbgud@naver.com <rnjsrbgud@naver.com>
2022-07-16 13:48:27 +0900

    api instance 생성

--- src/api.ts
+++ src/api.ts
@@ -3,28 +3,25 @@ import axios from "axios";
 const BASE_URL = "https://api.coinpaprika.com/v1";
 const PRICEURL = `https://ohlcv-api.nomadcoders.workers.dev/?coinId=`;
 
-const instance = axios.create({
-  baseURL: "https://api.coinpaprika.com/v1",
-  timeout:500,
-})
-
+const customAxios = axios.create({
+  baseURL: ``, // 기본 서버 주소 입력
+  timeout: 500,
+});
 // 이건 함수형으로 해줘야함
 export const fetchCoins = () =>{
   // 데이터 얻는법
   // axios.get(url).then(res => console.log(res.data))
-  return instance.get("/coins").then((res) => res.data);
+  return customAxios.get(`${BASE_URL}/coins`).then((res) => res.data);
 }
 // coin 화면 api
 export const coinUrl = (coinId:string) =>{
-  const response = axios.get(`${BASE_URL}/coins/${coinId}`).then(res => res.data)
-  return response;
+  return customAxios.get(`${BASE_URL}/coins/${coinId}`).then((res) => res.data);
+  
 }
 export const priceUrl = (coinId:string) =>{
-  const response = axios.get(`${BASE_URL}/tickers/${coinId}`).then(res => res.data)
-  return response;
+  return customAxios.get(`${BASE_URL}/tickers/${coinId}`).then((res) => res.data);;
 }
 // 차트 화면
 export const chartCoin = (coinId:string) => {
-  const response = axios.get(`${PRICEURL}${coinId}`).then((res) => res.data);
-  return response;
+  return customAxios.get(`${PRICEURL}${coinId}`).then((res) => res.data);
 }
