58715228f5f7b9968d6b3e68b7f3b11726f82530
rnjsrbgud@naver.com <rnjsrbgud@naver.com>
2022-07-20 12:06:26 +0900

    설명요약

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ const Container = styled.main`
 `;
 function App() {
   const isDark = useRecoilValue(isDarkAtom);
+  // 리코일의 장ㅓ
   // const [isDark , setIsDark] = useState(false);
   // const toggleDark = () =>{
   //   // value 대신 function 을 보내서 스테이트 값을 변경해줌
